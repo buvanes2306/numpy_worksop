64f1435714920c89e45a75591b3f09c47ed13640
buvanes2306 <buvanes2306@gmail.com>
2024-05-22 12:05:09 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,7 @@
 #write a program to find the maximum of two numbers
+n1 = int(input("Enter the number:"))
+n2 = int(input("Enter the number:"))
+if(n1 > n2):
+  print("1st number is greater")
+else:
+  print("2nd number is greater")
