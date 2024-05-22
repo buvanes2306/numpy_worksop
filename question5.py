d5cd86d02588dd7facf454f995da6dc01f144e12
buvanes2306 <buvanes2306@gmail.com>
2024-05-22 11:59:01 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,1 +1,12 @@
 #write a program to find if the given number is prime no or not
+def is_prime(n):
+    if n <= 1:
+        return False
+    for divisor in range(2, n//2 + 1):
+        if n % divisor == 0:
+            print(f"{n} is not a prime number")
+            return
+    print(f"{n} is a prime number")
+num = int(input("Enter the number: "))
+is_prime(num)    
+          
