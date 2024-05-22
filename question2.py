55e62e4a5167173e6b63a8eb9682bf1df872c58a
buvanes2306 <buvanes2306@gmail.com>
2024-05-22 10:05:21 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,6 @@
 # find if the given number is a palindrome or not?
+n = input("enter a string")
+if(n == n[::-1]):
+  print("the given string is plaindrome")
+else:
+  print("the given string is not palindrome")
